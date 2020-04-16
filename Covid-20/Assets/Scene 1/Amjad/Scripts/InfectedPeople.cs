ea982c3c3c69219876afaadc564f77ab89f6d971
Khalid Hasasn <khalidhsoliman@gmail.com>
2020-04-16 02:04:24 +0200

    Navigation AI for the NPCs

--- Covid-20/Assets/Scene 1/Amjad/Scripts/InfectedPeople.cs
+++ Covid-20/Assets/Scene 1/Amjad/Scripts/InfectedPeople.cs
@@ -1,27 +1,61 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.AI; 
 
 public class InfectedPeople : MonoBehaviour
 {
-    public Transform player;
-    private float movementSpeed ;
 
+    public float patrolTime = 15;
+    public float aggroRange = 10;
+    public Transform[] waypoints; 
+
+    int index;
+    float speed, agentSpeed; 
+
+    Animator animator; 
+    NavMeshAgent agent; 
+    Transform player;
+
+    private float movementSpeed ;
 
     private void Awake()
     {
-        transform.position = new Vector3(Random.Range(-43 , 43), 0, Random.Range(20 , 100));
-    }
-
-    void Start()
-    {
+        // transform.position = new Vector3(Random.Range(-43 , 43), 0, Random.Range(20 , 100));
+        animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        if (agent != null) { agentSpeed = agent.speed; }
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        index = Random.Range(0, waypoints.Length);
         movementSpeed = Random.Range(15, 18);
+
+        InvokeRepeating("Tick", 0, 0.5f);
+
+        if (waypoints.Length > 0)
+        {
+            InvokeRepeating("Patrol", Random.Range(0, patrolTime), patrolTime);
+        }
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        if (Vector3.Distance(this.transform.position, player.position) < 15.0f)
-        transform.position = Vector3.MoveTowards(transform.position, player.position, 0.01f * movementSpeed);
+        if(animator)
+            animator.SetFloat("Speed", agent.velocity.magnitude);
     }
+
+    void Patrol()
+    {
+        index = index == waypoints.Length - 1 ? 0 : index + 1;
+    }
+
+    void Tick()
+    {
+        agent.destination = waypoints[index].position;
+        agent.speed = agentSpeed / 2;
+
+        if (player != null && Vector3.Distance(transform.position, player.position) < aggroRange)
+        {
+            agent.destination = player.position;
+            agent.speed = agentSpeed;
+        }
+    }
+
 }
